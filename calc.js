4a2036da7391a6f68bcd226240d94018615e4243
Dylan Redman <dylan.j.redman@gmail.com>
2018-07-28 09:50:02 -0500

    Added functions to set display numbers and set operator

--- calc.js
+++ calc.js
@@ -1,3 +1,32 @@
+var numberSection = document.querySelector('.numberSection');
+numberSection.addEventListener('click', display, false);
+
+var operatorSection = document.querySelector('.operatorSection');
+operatorSection.addEventListener('click', setOperator, false);
+
+var results = document.querySelector('#results');
+
+var output, operator;
+
+function display(e){
+    if(e.target.classList.contains('number')){
+        output = e.target.textContent;
+        results.textContent = output;
+    }
+}
+
+function setOperator(e){
+    if(e.target.textContent === "/"){
+        operator = "divide";
+    }else if(e.target.textContent === "X"){
+        operator = "multiply";
+    }else if (e.target.textContent === "-"){
+        operator = "subtract";
+    }else if(e.target.textContent === "+"){
+        operator = "add";
+    }
+}
+
 function operate(operator, x, y){
     if(operator === "add"){
         add(x,y);
